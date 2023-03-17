90a28d3e35f47e748d5e99e493766e3884f35c15
kawamoto <kawamoto@system-age.co.jp>
2023-03-17 15:32:37 +0900

    initialize

---
+++ src/main/java/mix/famick/FamickApplication.java
@@ -0,0 +1,13 @@
+package mix.famick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mix/famick/FamickApplicationTests.java
@@ -0,0 +1,13 @@
+package mix.famick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
