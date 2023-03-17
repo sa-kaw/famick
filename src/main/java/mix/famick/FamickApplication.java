6a694661123d9e2a3d62279f93b88624cbef0fd3
kawamoto <kawamoto@system-age.co.jp>
2023-03-17 15:35:52 +0900

    editorconfig

--- src/main/java/mix/famick/FamickApplication.java
+++ src/main/java/mix/famick/FamickApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FamickApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FamickApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(FamickApplication.class, args);
+  }
 
 }
